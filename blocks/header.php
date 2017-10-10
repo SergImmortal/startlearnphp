7c732276fabb932827df74598191896b600afc6a
Sergey Maksimenko <s.maxymenko@gmail.com>
2017-10-10 09:21:03 +0000

    MSG

--- blocks/header.php
+++
@@ -1,8 +0,0 @@
-    <header>
-        <div id="logo">
-            <i class="fa fa-camera-retro" aria-hidden="true"></i> <span>MyPhotoGalery</span>
-        </div>
-        <div id="Author">
-            <a href = "https://startlearnphp-maksis.c9users.io/admin/adminmain.php"><i class="fa fa-lock" aria-hidden="true"></i></a>
-        </div>
-    </header>
