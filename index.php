d9b84b5eb276f8ea75cffb0ec55feb0d62b8abc1
Sergey Maksimenko <s.maxymenko@gmail.com>
2017-10-10 09:15:03 +0000

    Add simple Router

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
- echo __FILE__;
+
+
+ 
+$query = rtrim($_SERVER['QUERY_STRING'], '/');
+require '/home/ubuntu/workspace/vendor/core/Router.php';
+require '/home/ubuntu/workspace/vendor/libs/functions.php';
+
+Router::add('post/add', ['controller' => 'Post', 'action' => 'add']);
+Router::add('post', ['controller' => 'Post', 'action' => 'index']);
+Router::add('', ['controller' => 'Main', 'action' => 'index']);
+//printArray(Router::getRoutes());
+
+
+if(Router::matchRoute($query)){
+    printArray(Router::getRoute());
+}else{
+    echo '404';
+};
+
 ?>
---
+++ vendor/libs/functions.php
@@ -0,0 +1,6 @@
+<?php
+function printArray($arr) {
+    echo '<pre>' . print_r($arr, true) . '</pre>';
+}
+
+?>
